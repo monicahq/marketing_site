296055baa9ad63565530cdd7e74744181b54b346
Regis Freyd <djaiss@users.noreply.github.com>
2018-01-02 18:32:46 -0500

    first commit

---
+++ resources/views/partials/skeleton.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  @include('partials.header')
+
+  <body>
+
+    @yield('content')
+
+    @include ('partials.footer')
+
+  </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+@extends('partials.skeleton')
+
+@section('content')
+
+<div class="">
+  <h1>Personal Relationship Management done right.</h1>
+  <h2>Monica helps you organize the social interactions with your loved ones.</h2>
+  <p>It's like your social memory. Organize. Follow-up. Strenghen your relationships.</p>
+  <div class="">
+    <a href="">Signup for free</a>
+    <p>No obligations. No credit card required.</p>
+  </div>
+  <div class="">
+    <p>We're delighted that 13,023 users signed up already.</p>
+  </div>
+  <p>What is Monica?</p>
+  <p>Can you remember the names of the children of all your friends? Can you remember the wedding anniversary of your brother? Can you tell the last time you called your grand mother and what you talked about? Monica lets you quickly and easily log all those information so you can be a better friend, family member or spouse.</p>
+  <p>Your personal rolodex</p>
+  <p>Everything in one place</p>
+  <p>The more information you put in Monica, the easier it will be to remember everything. Monica provides an easy-to-use interface to store everything you know about your loved ones.</p>
+  <ul>
+    <li>Information about family</li>
+    <li>Work information</li>
+    <li>Relation between contacts</li>
+    <li>Pets</li>
+    <li>Activites done together</li>
+    <li>Reminders about important dates</li>
+    <li>Gift ideas or gifts you've made</li>
+    <li>Tasks</li>
+    <li>Debts you owe or owned</li>
+    <li>Journal</li>
+    <li>Indicate how your day went</li>
+  </ul>
+  <h2>Monica powers people who want to be better</h2>
+  <ul>
+    <li>10300 users</li>
+    <li>98000 contacts managed</li>
+  </ul>
+  <p>Monica is for people who have jobs, a family, and are busy trying to find a good work/life balance. So busy, that they don’t have time anymore to remember to call a friend, say happy birthday to a nephew, or remember to invite someone special for dinner next week. The older we get, the more life gets in the way. It’s sad, but it’s the reality. I created Monica to help these people.</p>
+  <h3>Built for people. With people.</h3>
+  <p>Monica is open source. That means the code is publicly available, scrutinized by thousands. Dozens of people have already contributed to the project. That level of transparency ensures both the security of the application, and your peace of mind concerning what we do with your data (nothing, in fact).</p>
+</div>
+
+@endsection

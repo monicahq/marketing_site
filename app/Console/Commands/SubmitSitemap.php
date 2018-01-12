e11b456e462f9bd57458f44b514390ab0a73c1ad
Regis Freyd <djaiss@users.noreply.github.com>
2018-01-12 10:33:17 -0500

    Add sitemap submitting

---
+++ app/Console/Commands/SubmitSitemap.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Ixudra\Curl\Facades\Curl;
+use Illuminate\Console\Command;
+
+class SubmitSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap:submit';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Submits the sitemap to the big 3.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $sitemapUrl = "https://www.monicahq.com/sitemap/sitemap.xml";
+
+        $url = "http://www.google.com/webmasters/sitemaps/ping?sitemap=".$sitemapUrl;
+        $response = Curl::to($url)->get();
+
+        //Bing / MSN
+        $url = "http://www.bing.com/webmaster/ping.aspx?siteMap=".$sitemapUrl;
+        $response = Curl::to($url)->get();
+
+        //ASK
+        $url = "http://submissions.ask.com/ping?sitemap=".$sitemapUrl;
+        $response = Curl::to($url)->get();
+    }
+}

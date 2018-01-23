e503de0472849f7ddf67dacf45454a812ee422c0
Regis Freyd <djaiss@users.noreply.github.com>
2018-01-22 23:18:37 -0500

    Add RSS feed

---
+++ config/feed.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    'feeds' => [
+        'main' => [
+            /*
+             * Here you can specify which class and method will return
+             * the items that should appear in the feed. For example:
+             * 'App\Model@getAllFeedItems'
+             *
+             * You can also pass an argument to that method:
+             * ['App\Model@getAllFeedItems', 'argument']
+             */
+            'items' => 'App\Post@getFeedItems',
+
+            /*
+             * The feed will be available on this url.
+             */
+            'url' => '/feed',
+
+            'title' => 'All the articles on Monica',
+        ],
+    ],
+
+];

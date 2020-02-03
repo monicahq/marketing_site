8815de512939d424ba7260a61b4e4b512592e54c
Regis Freyd <djaiss@users.noreply.github.com>
2020-02-03 16:02:49 -0500

    chore: add analytics

--- resources/views/partials/skeleton_blog.blade.php
+++ resources/views/partials/skeleton_blog.blade.php
@@ -1,52 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>{{ $pageTitle }}</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>{{ $pageTitle }}</title>
 
-    <link rel="canonical" href="{{ secure_url('/') }}">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <link rel="alternate" type="application/rss+xml" title="{{ trans('marketing.xml_feed_description') }}" href="{{ url("/feed.xml") }}">
-    <meta name="description" content="{{ trans('marketing.description') }}">
-    <link rel="author" href="https://twitter.com/djaiss">
-    <link rel="shortcut icon" href="{{ secure_url('/img/favicon.png') }}">
-    <link rel="alternate" type="application/atom+xml" title="News" href="/feed">
+  <link rel="canonical" href="{{ secure_url('/') }}">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  <link rel="alternate" type="application/rss+xml" title="{{ trans('marketing.xml_feed_description') }}" href="{{ url("/feed.xml") }}">
+  <meta name="description" content="{{ trans('marketing.description') }}">
+  <link rel="author" href="https://twitter.com/djaiss">
+  <link rel="shortcut icon" href="{{ secure_url('/img/favicon.png') }}">
+  <link rel="alternate" type="application/atom+xml" title="News" href="/feed">
 
-    <!-- Facebook -->
+  <!-- Facebook -->
 
-    <!-- Between 60 and 80 char. Cut at 88. -->
-    <meta property="og:title" content="{{ $post->title }}" />
-    <meta property="og:url" content="{{ secure_url('/blog/'.$post->slug) }}" />
-    <meta property="og:type" content="website" />
-    <meta property="og:site_name" content="{{ config('app.name') }}" />
-    <meta property="og:description" content="{{ $post->social_media_description }}" />
-    <meta property="og:locale" content="en_US" />
-    <meta property="og:image:type" content="image/png" />
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="630" />
-    <meta property="article:published_time" content="{{ $post->created_at->format('Y-m-d') }}" />
-    <meta property="og:image" content="{{ secure_url('/img/social/posts/'.$post->created_at->format('Y-m-d').'/facebook.png') }}" />
-    <meta property=”fb:admins content="regis.freyd" />
+  <!-- Between 60 and 80 char. Cut at 88. -->
+  <meta property="og:title" content="{{ $post->title }}" />
+  <meta property="og:url" content="{{ secure_url('/blog/'.$post->slug) }}" />
+  <meta property="og:type" content="website" />
+  <meta property="og:site_name" content="{{ config('app.name') }}" />
+  <meta property="og:description" content="{{ $post->social_media_description }}" />
+  <meta property="og:locale" content="en_US" />
+  <meta property="og:image:type" content="image/png" />
+  <meta property="og:image:width" content="1200" />
+  <meta property="og:image:height" content="630" />
+  <meta property="article:published_time" content="{{ $post->created_at->format('Y-m-d') }}" />
+  <meta property="og:image" content="{{ secure_url('/img/social/posts/'.$post->created_at->format('Y-m-d').'/facebook.png') }}" />
+  <meta property=”fb:admins content="regis.freyd" />
 
-    <!-- META SPECIFIC TO TWITTER -->
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:site" content="@MonicaHQ_app">
-    <meta name="twitter:creator" content="@djaiss">
-    <meta name="twitter:title" content="{{ $post->title }}">
-    <meta name="twitter:description" content="{{ $post->social_media_description }}">
-    <meta property="og:image" content="{{ secure_url('/img/social/posts/'.$post->created_at->format('Y-m-d').'/twitter.png') }}" />
+  <!-- META SPECIFIC TO TWITTER -->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@MonicaHQ_app">
+  <meta name="twitter:creator" content="@djaiss">
+  <meta name="twitter:title" content="{{ $post->title }}">
+  <meta name="twitter:description" content="{{ $post->social_media_description }}">
+  <meta property="og:image" content="{{ secure_url('/img/social/posts/'.$post->created_at->format('Y-m-d').'/twitter.png') }}" />
 
-  </head>
+</head>
 
-  <body class="sans-serif">
-    @include ('partials.menu')
+<body class="sans-serif">
+  @include ('partials.menu')
 
-    @yield('content')
+  @yield('content')
 
-    @include ('partials.footer')
+  @include ('partials.footer')
 
-  </body>
+  <!-- Fathom - simple website analytics - https://usefathom.com -->
+  <script>
+    (function(f, a, t, h, o, m) {
+      a[h] = a[h] || function() {
+        (a[h].q = a[h].q || []).push(arguments)
+      };
+      o = f.createElement('script'),
+        m = f.getElementsByTagName('script')[0];
+      o.async = 1;
+      o.src = t;
+      o.id = 'fathom-script';
+      m.parentNode.insertBefore(o, m)
+    })(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
+    fathom('set', 'siteId', 'TZPOGJXQ');
+    fathom('trackPageview');
+  </script>
+  <!-- / Fathom -->
+</body>
+
 </html>

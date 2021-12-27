4fd20a2ddbb113f77e027bbcc520e6042d4f923a
Alexis Saettler <alexis@saettler.org>
2021-12-27 12:01:35 +0100

    chore: readd monicahq/laravel-cloudflare

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,27 +8,6 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        //
-    ];
-
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')
-        //          ->hourly();
-    }
-
-    /**
      * Register the commands for the application.
      *
      * @return void
@@ -38,5 +17,18 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
+    }
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        if (config('trustedproxy.cloudflare')) {
+            $schedule->command('cloudflare:reload')->daily(); // @codeCoverageIgnore
+        }
     }
 }
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,8 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        //\Monicahq\Cloudflare\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\TrustProxies::class,
+        \Monicahq\Cloudflare\Http\Middleware\TrustProxies::class,
         \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,

3408b7681528c2beb7be258bb406d13b82022902
Regis Freyd <djaiss@users.noreply.github.com>
2022-03-31 17:42:05 -0400

    chore: add refund page (#750)

---
+++ resources/views/refund.blade.php
@@ -0,0 +1,24 @@
+@extends('partials.skeleton', ['pageTitle' => 'Personal CRM - Monica privacy page'])
+
+@section('content')
+
+<div class="mw8 center ph3 mt4">
+  <div class="cf">
+    <div class="fl w-100 tc mb2">
+      <h1 class=" normal mb2">Our fair refund policy</h1>
+      <p class="normal lh-copy measure center">Last update: Mar 31, 2022</p>
+    </div>
+  </div>
+</div>
+
+<div class="mw8 center ph3 mb6">
+  <div class="cf">
+    <div class="w-100 w-60-ns center">
+      <p class="measure lh-copy">We aim to have the simplest refund policy as possible: if you are unhappy with the product, for any reason, just contact us and we'll take care of you.</p>
+      <p class="measure lh-copy">If you were just charged for your next month of service but you meant to cancel, we’re happy to refund that extra charge.</p>
+      <p class="measure lh-copy">If you were just charged for your next year of service but meant to cancel, we're also happy to refund the extra charge.</p>
+      <p class="measure lh-copy">However, please keep in mind that we are still a side project, and that every refund is costly for us. So while we'll happily refund, we try to avoid it as much as possible.</p>
+    </div>
+  </div>
+</div>
+@endsection

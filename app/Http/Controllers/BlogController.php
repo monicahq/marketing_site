1d59eb36e5fe64dbd9ceac23d063d2c4c30b3b26
Mazarin <djaiss@users.noreply.github.com>
2023-01-19 09:57:17 -0500

    feat: add privacy first analytics (#961)

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -12,7 +12,6 @@ class BlogController extends Controller
      * Display a listing of the resource.
      *
      * @param Request $request
-     * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
     {

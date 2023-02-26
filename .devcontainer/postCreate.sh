ffccac46ac7ed7e94c9b14600e0c53bfcca82eab
Alexis Saettler <alexis@saettler.org>
2023-02-26 19:01:51 +0100

    build: add devcontainer (#993)

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+DATABASE=database/database.sqlite
+
+setenv() {
+    sed -i "s%$1=.*%$1=$2%" .env
+}
+
+set_apache() {
+    chgrp -R www-data storage && chmod -R g+w storage
+    sudo rm -rf /var/www/html && sudo ln -s "$(pwd)/public" /var/www/html
+    sudo a2enmod rewrite
+    sudo service apache2 restart
+}
+
+set_database() {
+    cp .env.example .env && echo "APP_TRUSTED_PROXIES=*" >> .env
+    setenv "DB_CONNECTION" "sqlite"
+    setenv "DB_DATABASE" "$(pwd)/$DATABASE"
+    touch $DATABASE && chgrp www-data database $DATABASE && chmod g+w database $DATABASE
+}
+
+set_conf() {
+    setenv "CACHE_DRIVER" "database"
+    setenv "QUEUE_CONNECTION" "sync"
+    setenv "SESSION_DRIVER" "database"
+    setenv "MAIL_MAILER" "log"
+    setenv "MAIL_FROM_ADDRESS" "from@mail.com"
+    setenv "MAIL_REPLY_TO_ADDRESS" "reply@mail.com"
+}
+
+composer_install() {
+    composer install --no-progress --no-interaction --prefer-dist --optimize-autoloader
+}
+
+yarn_install() {
+    yarn install --frozen-lockfile
+    yarn run production
+}
+
+setup() {
+    php artisan key:generate --no-interaction
+    php artisan setup --force -vvv
+}
+
+set_apache
+set_database
+set_conf
+composer_install
+setup
+yarn_install

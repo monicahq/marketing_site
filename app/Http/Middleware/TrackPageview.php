7746e689e5ef7d245221f5a4c1a5964c7fedb65c
Alexis Saettler <alexis@saettler.org>
2024-03-10 11:22:46 +0100

    Add logs (#1048)
    * Add logs
    
    * update

--- app/Http/Middleware/TrackPageview.php
+++ app/Http/Middleware/TrackPageview.php
@@ -41,6 +41,13 @@ class TrackPageview
 
         app()->terminating(function () use ($name, $meta) {
 
+            Log::info('Pirsch '.($name === null ? 'hit' : 'event'), [
+                'name' => $name,
+                'meta' => $meta,
+                'url' => request()->fullUrl(),
+                'ip' => request()->ip(),
+            ]);
+
             try {
                 Http::withToken(config('pirsch.token'))
                     ->timeout(5)
